e32a345b6b875bafeea35746c693575ff7380add
Kyle Spiller <me@kylespiller.com>
2021-04-16 00:29:14 -0400

    Another attempt at setting the entrypoint

--- dev-entrypoint.sh
+++ dev-entrypoint.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-echo "xdebug.mode = debug
-xdebug.client_host = 192.168.1.111
-xdebug.client_port = 9003" >> /usr/local/etc/php/conf.d/dev.ini
 
 exec "$@"
